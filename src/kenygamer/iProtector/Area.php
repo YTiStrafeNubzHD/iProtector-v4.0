49a90e8aa2c3eda17e73584e8cb91b16f682beb0
iStrafeNubzHDyt <pixelgameskontakt@gmail.com>
2018-10-29 17:29:22 +0100

    Update Area.php

--- src/kenygamer/iProtector/Area.php
+++ src/kenygamer/iProtector/Area.php
@@ -1,239 +1,192 @@
 <?php
 
-/*
- * iProtector-v4.0 plugin for PocketMine-MP
- * Copyright (C) 2017 Kevin Andrews <https://github.com/kenygamer/pmmp-plugins/blob/master/iProtector-v4.0>
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
-*/
+declare(strict_types = 1);
 
-namespace kenygamer\iProtector;
+namespace kenygamer\iProtector; 
 
+use pocketmine\level\Level;
 use pocketmine\math\Vector3;
-use pocketmine\Player;
 
 class Area{
-  
-  const WHITELIST_ACTION_ADD = 0;
-  const WHITELIST_ACTION_REMOVE = 1;
-  
-  /** @var string */
-  private $name;
-  /** @var array */
-  private $flags;
-  /** @var Vector3 */
-  private $pos1;
-  /** @var Vector3 */
-  private $pos2;
-  /** @var string */
-  private $level;
-  /** @var string[] */
-  private $whitelist;
-  /** @var Main */
-  private $plugin;
-  
-  public function __construct(string $name, array $flags, array $pos1, array $pos2, string $level, array $whitelist, Main $plugin){
-    $this->name = strtolower($name);
-    $this->flags = $flags;
-    $this->pos1 = new Vector3($pos1[0], $pos1[1], $pos1[2]);
-    $this->pos2 = new Vector3($pos2[0], $pos2[1], $pos2[2]);
-    $this->level = $level;
-    $this->whitelist = $whitelist;
-    $this->plugin = $plugin;
-    $this->save();
-  }
-  
-  /**
-   * Returns the area name
-   *
-   * @return string
-   */
-  public function getName() : string{
-    return $this->name;
-  }
-  
-  /**
-   * Returns area 1st positions
-   *
-   * @return array
-   */
-  public function getPos1() : array{
-    return [
-      $this->pos1->getX(),
-      $this->pos1->getY(),
-      $this->pos1->getZ()
-      ];
-  }
-  
-  /**
-   * Returns area 2nd positions
-   *
-   * @return array
-   */
-  public function getPos2() : array{
-    return [
-      $this->pos2->getX(),
-      $this->pos2->getY(),
-      $this->pos2->getZ()
-      ];
-  }
-  
-  /**
-   * Returns area flags
-   *
-   * @return array
-   */
-  public function getFlags() : array{
-    return $this->flags;
-  }
-  
-  /**
-   * Returns area flag value
-   *
-   * @param string $flag
-   *
-   * @return bool
-   */
-  public function getFlag(string $flag) : bool{
-    if(isset($this->flags[$flag])){
-      return $this->flags[$flag];
-    }
-    return false;
-  }
-  
-  /**
-   * Sets area flag to given value
-   *
-   * @param string $flag
-   * @param bool $value
-   *
-   * @return bool
-   */
-  public function setFlag(string $flag, bool $value) : bool{
-    if(isset($this->flags[$flag])){
-      $this->flags[$flag] = $value;
-      $this->plugin->saveAreas();
-      return true;
-    }
-    return false;
-  }
-  
-  /**
-   * Toggles area flag value
-   *
-   * @param string $flag
-   *
-   * @return bool
-   */
-  public function toggleFlag(string $flag) : bool{
-    if(isset($this->flags[$flag])){
-      $this->flags[$flag] = !$this->flags[$flag];
-      $this->plugin->saveAreas();
-      return $this->flags[$flag];
-    }
-    return false;
-  }
-  
-  /**
-   * Checks if area is inside given position
-   *
-   * @param Vector3 $position
-   * @param string $level
-   *
-   * @return bool
-   */
-  public function contains(Vector3 $position, string $level) : bool{
-    if((min($this->pos1->getX(), $this->pos2->getX()) <= $position->getX()) && (max($this->pos1->getX(), $this->pos2->getX()) >= $position->getX()) && (min($this->pos1->getY(), $this->pos2->getY()) <= $position->getY()) && (max($this->pos1->getY(), $this->pos2->getY()) >= $position->getY()) && (min($this->pos1->getZ(), $this->pos2->getZ()) <= $position->getZ()) && (max($this->pos1->getZ(), $this->pos2->getZ()) >= $position->getZ()) && ($this->level == $level)) {
-      return true;
-    }
-    return false;
-  }
-  
-  /**
-   * Returns area level name
-   *
-   * @return string
-   */
-  public function getLevel() : string{
-    return $this->level;
-  }
-  
-  /**
-   * Checks if given player is whitelisted
-   *
-   * @param string $player
-   *
-   * @return bool
-   */
-  public function isWhitelisted(string $player) : bool{
-    if(in_array($player, $this->whitelist)){
-      return true;
-    }
-    return false;
-  }
-  
-  /**
-   * Adds/removes player from area whitelist
-   *
-   * @param string $player
-   * @param bool $action
-   *
-   * @return bool
-   */
-  public function setWhitelisted(string $player, bool $action) : bool{
-    if($action === self::WHITELIST_ACTION_ADD){
-      if(!in_array($player, $this->whitelist)){
-        array_push($this->whitelist, $player);
-        $this->plugin->saveAreas();
-        return true;
-      }
-    }elseif($action === self::WHITELIST_ACTION_REMOVE){
-      if(in_array($player, $this->whitelist)){
-        $key = array_search($player, $this->whitelist);
-        array_splice($this->whitelist, $key, 1);
-        $this->plugin->saveAreas();
-        return true;
-      }
-    }
-    return false;
-  }
-  
-  /**
-   * Returns area whitelist
-   *
-   * @return string[]
-   */
-  public function getWhitelist() : array{
-    return $this->whitelist;
-  }
-  
-  /**
-   * Saves the area
-   *
-   * @return bool
-   */
-  public function save() : bool{
-    $this->plugin->areas[$this->name] = $this;
-    return true;
-  }
-  
-  /**
-   * Deletes the area
-   *
-   * @return bool
-   */
-  public function delete() : bool{
-    unset($this->plugin->areas[$this->getName()]);
-    $this->plugin->saveAreas();
-    return true;
-  }
-  
+
+	/** @var bool[] */
+	public $flags;
+	/** @var string */
+	private $name;
+	/** @var Vector3 */
+	private $pos1;
+	/** @var Vector3 */
+	private $pos2;
+	/** @var string */
+	private $levelName;
+	/** @var string[] */
+	private $whitelist;
+	/** @var Main */
+	private $plugin;
+
+	public function __construct(string $name, array $flags, Vector3 $pos1, Vector3 $pos2, string $levelName, array $whitelist, Main $plugin){
+		$this->name = strtolower($name);
+		$this->flags = $flags;
+		$this->pos1 = $pos1;
+		$this->pos2 = $pos2;
+		$this->levelName = $levelName;
+		$this->whitelist = $whitelist;
+		$this->plugin = $plugin;
+		$this->save();
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getName() : string {
+		return $this->name;
+	}
+
+	/**
+	 * @return Vector3
+	 */
+	public function getFirstPosition() : Vector3{
+		return $this->pos1;
+	}
+
+	/**
+	 * @return Vector3
+	 */
+	public function getSecondPosition() : Vector3{
+		return $this->pos2;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getFlags() : array{
+		return $this->flags;
+	}
+
+	/**
+	 * @param string $flag
+	 *
+	 * @return bool
+	 */
+	public function getFlag(string $flag) : bool{
+		if(isset($this->flags[$flag])){
+			return $this->flags[$flag];
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param string $flag
+	 * @param bool   $value
+	 *
+	 * @return bool
+	 */
+	public function setFlag(string $flag, bool $value) : bool{
+		if(isset($this->flags[$flag])){
+			$this->flags[$flag] = $value;
+			$this->plugin->saveAreas();
+
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param Vector3 $pos
+	 * @param string  $levelName
+	 *
+	 * @return bool
+	 */
+	public function contains(Vector3 $pos, string $levelName) : bool{
+		return ((min($this->pos1->getX(), $this->pos2->getX()) <= $pos->getX()) && (max($this->pos1->getX(), $this->pos2->getX()) >= $pos->getX()) && (min($this->pos1->getY(), $this->pos2->getY()) <= $pos->getY()) && (max($this->pos1->getY(), $this->pos2->getY()) >= $pos->getY()) && (min($this->pos1->getZ(), $this->pos2->getZ()) <= $pos->getZ()) && (max($this->pos1->getZ(), $this->pos2->getZ()) >= $pos->getZ()) && ($this->levelName === $levelName));
+	}
+
+	/**
+	 * @param string $flag
+	 *
+	 * @return bool
+	 */
+	public function toggleFlag(string $flag) : bool{
+		if(isset($this->flags[$flag])){
+			$this->flags[$flag] = !$this->flags[$flag];
+			$this->plugin->saveAreas();
+
+			return $this->flags[$flag];
+		}
+
+		return false;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getLevelName() : string{
+		return $this->levelName;
+	}
+
+	/**
+	 * @return null|Level
+	 */
+	public function getLevel() : ?Level{
+		return $this->plugin->getServer()->getLevelByName($this->levelName);
+	}
+
+	/**
+	 * @param string $playerName
+	 *
+	 * @return bool
+	 */
+	public function isWhitelisted(string $playerName) : bool{
+		if(in_array($playerName, $this->whitelist)){
+			return true;
+		}
+
+		return false;
+	}
+
+	/**
+	 * @param string $name
+	 * @param bool   $value
+	 *
+	 * @return bool
+	 */
+	public function setWhitelisted(string $name, bool $value = true) : bool{
+		if($value){
+			if(!in_array($name, $this->whitelist)){
+				$this->whitelist[] = $name;
+				$this->plugin->saveAreas();
+
+				return true;
+			}
+		}else{
+			if(in_array($name, $this->whitelist)){
+				$key = array_search($name, $this->whitelist);
+				array_splice($this->whitelist, $key, 1);
+				$this->plugin->saveAreas();
+
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+	/**
+	 * @return string[]
+	 */
+	public function getWhitelist() : array{
+		return $this->whitelist;
+	}
+
+	public function delete() : void{
+		unset($this->plugin->areas[$this->getName()]);
+		$this->plugin->saveAreas();
+	}
+
+	public function save() : void{
+		$this->plugin->areas[$this->name] = $this;
+	}
+
 }
